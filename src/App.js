a015f2b6c948117220f4128eddbdff365cc5d5a7
Iasonas-Pap <jason.pap@hotmail.com>
2021-04-25 22:41:51 +0300

    First project commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ToDoList from './ToDoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialList : [],
+      outputList : []
+    }
+    this.filterList = this.filterList.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then((response) => response.json())
+    .then((json) => {
+      const initialList = json.map( ({id,title}) => 
+        <li id="to-do-list-item" key={id}><span id="list-item-id">{id}</span>{title}</li>
+      );
+      this.setState({initialList: initialList, outputList: initialList})
+    });    
+  }
+
+  filterList(event) {    
+    this.setState({
+      outputList: this.state.initialList.filter((listItem) => {
+        if(JSON.stringify(listItem).includes(event.target.value)) return true;
+      })
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 id="title">
+            To Do List
+          </h1>
+          <input id="filter-list" type="text" onChange={this.filterList} />
+          <ul className="to-do-list">
+            {this.state.outputList}
+          </ul>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/ToDoList.js
@@ -0,0 +1,22 @@
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+    constructor(props) {
+        
+    }
+    render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1>
+              To Do List
+            </h1>
+            <input></input>
+          </header>
+        </div>
+      )
+    }
+  }
+  
+  export default App;
