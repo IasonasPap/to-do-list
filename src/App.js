3fef05fb736765a0f836685af3848ca5b4b8170c
Iasonas-Pap <jason.pap@hotmail.com>
2021-04-25 23:23:55 +0300

    Split App component into two components

--- src/App.js
+++ src/App.js
@@ -3,34 +3,7 @@ import React from 'react';
 import ToDoList from './ToDoList';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      initialList : [],
-      outputList : []
-    }
-    this.filterList = this.filterList.bind(this);
-  }
-
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-    .then((response) => response.json())
-    .then((json) => {
-      const initialList = json.map( ({id,title}) => 
-        <li id="to-do-list-item" key={id}><span id="list-item-id">{id}</span>{title}</li>
-      );
-      this.setState({initialList: initialList, outputList: initialList})
-    });    
-  }
-
-  filterList(event) {    
-    this.setState({
-      outputList: this.state.initialList.filter((listItem) => {
-        if(JSON.stringify(listItem).includes(event.target.value)) return true;
-      })
-    });
-  }
-
+  
   render() {
     return (
       <div className="App">
@@ -38,10 +11,7 @@ class App extends React.Component {
           <h1 id="title">
             To Do List
           </h1>
-          <input id="filter-list" type="text" onChange={this.filterList} />
-          <ul className="to-do-list">
-            {this.state.outputList}
-          </ul>
+          <ToDoList />
         </header>
       </div>
     )
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,22 +1,45 @@
 import './App.css';
 import React from 'react';
 
-class App extends React.Component {
+class ToDoList extends React.Component {
     constructor(props) {
-        
+      super(props);
+      this.state = {
+        initialList : [],
+        outputList : []
+      }
+      this.filterList = this.filterList.bind(this);
     }
+  
+    componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((response) => response.json())
+      .then((json) => {
+        const initialList = json.map( ({id,title}) => 
+          <li id="to-do-list-item" key={id}><span id="list-item-id">{id}</span>{title}</li>
+        );
+        this.setState({initialList: initialList, outputList: initialList})
+      });    
+    }
+  
+    filterList(event) {    
+      this.setState({
+        outputList: this.state.initialList.filter((listItem) => {
+          if(JSON.stringify(listItem).includes(event.target.value)) return true;
+        })
+      });
+    }
+  
     render() {
       return (
-        <div className="App">
-          <header className="App-header">
-            <h1>
-              To Do List
-            </h1>
-            <input></input>
-          </header>
+        <div class="to-do-list-container">     
+            <input id="filter-list" type="text" onChange={this.filterList} />
+            <ul className="to-do-list">
+                {this.state.outputList}
+            </ul>
         </div>
       )
     }
   }
   
-  export default App;
+  export default ToDoList;
